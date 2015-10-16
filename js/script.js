ecef2a032b4eb2b7abe838f8ae403cd4567b3452
Amanda Astey <aastey@aastey-06017.local>
2015-10-16 09:54:53 -0700

    newest changes to ashleys site include selected links and better scaling

--- js/script.js
+++ js/script.js
@@ -1,96 +1,104 @@
+var danAshApp = angular.module('danAshApp', ['ngRoute']);
 
-    var danAshApp = angular.module('danAshApp', ['ngRoute']);
+// configureroutes
+danAshApp.config(function($routeProvider) {
+    $routeProvider
 
-    // configureroutes
-    danAshApp.config(function($routeProvider) {
-        $routeProvider
+    // route for the home page
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'mainController'
+    })
 
-            // route for the home page
-            .when('/', {
-                templateUrl : 'pages/home.html',
-                controller  : 'mainController'
-            })
+    // route for the ashley and dan page
+    .when('/ashdan', {
+        templateUrl: 'pages/ashdan.html',
+        controller: 'ashdanController'
+    })
 
-            // route for the ashley and dan page
-            .when('/ashdan', {
-                templateUrl : 'pages/ashdan.html',
-                controller  : 'ashdanController'
-            })
+    // route for the families page
+    .when('/families', {
+        templateUrl: 'pages/families.html',
+        controller: 'familiesController'
+    })
 
-            // route for the families page
-            .when('/families', {
-                templateUrl : 'pages/families.html',
-                controller  : 'familiesController'
-            })
+    // route for the gifts and dan page
+    .when('/gifts', {
+        templateUrl: 'pages/gifts.html',
+        controller: 'giftsController'
+    })
 
-            // route for the gifts and dan page
-            .when('/gifts', {
-                templateUrl : 'pages/gifts.html',
-                controller  : 'giftsController'
-            })
+    // route for the location page
+    .when('/location', {
+        templateUrl: 'pages/location.html',
+        controller: 'locationController'
+    })
+    // route for the packing page
+    .when('/packing', {
+        templateUrl: 'pages/packing.html',
+        controller: 'packingController'
+    })
 
-            // route for the location page
-            .when('/location', {
-                templateUrl : 'pages/location.html',
-                controller  : 'locationController'
-            })
-            // route for the packing page
-            .when('/packing', {
-                templateUrl : 'pages/packing.html',
-                controller  : 'packingController'
-            })
+    // route for the rsvp page
+    .when('/rsvp', {
+        templateUrl: 'pages/rsvp.html',
+        controller: 'rsvpController'
+    })
+    // route for the schedule page
+    .when('/schedule', {
+        templateUrl: 'pages/schedule.html',
+        controller: 'scheduleController'
+    })
 
-            // route for the rsvp page
-            .when('/rsvp', {
-                templateUrl : 'pages/rsvp.html',
-                controller  : 'rsvpController'
-            })
-            // route for the schedule page
-            .when('/schedule', {
-                templateUrl : 'pages/schedule.html',
-                controller  : 'scheduleController'
-            })
-
-            // route for the sleep page
-            .when('/sleep', {
-                templateUrl : 'pages/sleep.html',
-                controller  : 'sleepController'
-            });
+    // route for the sleep page
+    .when('/sleep', {
+        templateUrl: 'pages/sleep.html',
+        controller: 'sleepController'
     });
+});
 
-    // create the controller and inject Angular's $scope
-    danAshApp.controller('mainController', function($scope) {
-        
-    });
+// create the controller and inject Angular's $scope
+danAshApp.controller('mainController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".wedding-link").addClass("selected");
+});
 
-    danAshApp.controller('ashdanController', function($scope) {
-        
-    });
+danAshApp.controller('ashdanController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".ashdan-link").addClass("selected");
+});
 
-    danAshApp.controller('familiesController', function($scope) {
-        
-    });
+danAshApp.controller('familiesController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".families-link").addClass("selected");
+});
 
-    danAshApp.controller('giftsController', function($scope) {
-        
-    });
+danAshApp.controller('giftsController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".gifts-link").addClass("selected");
+});
 
-    danAshApp.controller('locationController', function($scope) {
-        
-    });
+danAshApp.controller('locationController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".location-link").addClass("selected");
+});
 
-    danAshApp.controller('packingController', function($scope) {
-        
-    });
+danAshApp.controller('packingController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".packing-link").addClass("selected");
+});
 
-    danAshApp.controller('rsvpController', function($scope) {
-        
-    });
+danAshApp.controller('rsvpController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".rsvp-link").addClass("selected");
+});
 
-    danAshApp.controller('scheduleController', function($scope) {
-        
-    });
+danAshApp.controller('scheduleController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".schedule-link").addClass("selected");
+});
 
-    danAshApp.controller('sleepController', function($scope) {
-        $scope.message = 'Contact us! JK. This is just a demo.';
-    });
+danAshApp.controller('sleepController', function($scope) {
+    $(".nav-link").removeClass("selected");
+    $(".sleep-link").addClass("selected");
+});
